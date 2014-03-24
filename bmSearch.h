2699a78a5a081d4f5a6eb58be207ed6fa61df801
XiaoH <nightwishxiaoh@gmail.com>
2014-03-25 01:03:04 +0800

    Create bmSearch.h

---
+++ bmSearch.h
@@ -0,0 +1,14 @@
+#ifndef _BM_SEARCH_H
+#define _BM_SEARCH_H
+
+
+/***ASCII list length***/
+
+#define ASCII_LIST_LENGTH 256
+
+/* Interface */
+
+extern int BMSearch(char *dest_str,char *pattern);
+
+
+#endif
