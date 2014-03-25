cc662428caa6dd1dc33178ef92a7d8f3861e9332
XiaoH <nightwishxiaoh@gmail.com>
2014-03-25 19:20:30 +0800

    Update and rename bmSearch.h to Arithmetic.h

---
+++ Arithmetic.h
@@ -0,0 +1,20 @@
+#ifndef _ARITHMETIC_H
+#define _ARITHMETIC_H
+
+
+/***ASCII list length***/
+
+#define ASCII_LIST_LENGTH 256
+
+
+
+/* Interfaces */
+
+extern int BMSearch(char *src,char *pattern);
+
+extern int KMPSearch(char *src,char *pattern);
+
+
+
+
+#endif
